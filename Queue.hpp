718f057a744ddb2300040893168dd8eca12a7339
Louis-Paul Cordier <lpdev@cordier.org>
2021-06-04 15:04:08 +0200

    Update documentation

--- Queue.hpp
+++ Queue.hpp
@@ -27,7 +27,7 @@ public:
     /**
      * Get message from the head of the queue.
      * Blocks until at least one message is available in the queue, or until timeout happens.
-     * If get() returns due to timeout, the returned Msg will have Msg ID MSG_TIMEOUT.
+     * If get() returns due to timeout, returns a nullptr.
      *
      * @param timeoutMillis How many ms to wait for message until timeout happens.
      *                      0 = wait indefinitely.
@@ -35,15 +35,16 @@ public:
     std::unique_ptr<Msg> get(int timeoutMillis = 0);
 
     /**
-    * Get message from the head of the queue. Returns an empty pointer if no 
-    * message is available.
+    * Get message from the head of the queue.
+    * Returns an empty pointer if no message is available.
     */
     std::unique_ptr<Msg> tryGet();
 
     /**
      * Make a request.
      * Call will block until response is given with respondTo().
-     *
+     * If request() returns due to timeout, returns a nullptr.
+     * 
      * @param msg Request message. Is put to the queue so it can be retrieved from it with get().
      * @param timeoutMillis How many ms to wait for response until timeout happens.
      *                      0 = wait indefinitely.
